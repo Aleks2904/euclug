ddbfd38a2db269f4ace6dae479b7dcaf1b5a3bbb
Aleks2904 <Garmatin171@yandex.ru>
2020-10-24 13:30:43 +0700

    v 1.0

--- app/js/presentation-swiper-content.js
+++ app/js/presentation-swiper-content.js
@@ -1,18 +1,18 @@
 
 const presentContent = [
     {
-        jpg: '../img/block-presentation/slider/slide1.jpg',
-        webp: '../img/block-presentation/slider/slide1-w.webp',
+        jpg: 'img/block-presentation/slider/slide1.jpg',
+        webp: 'img/block-presentation/slider/slide1-w.webp',
         alt: 'описание 1'
     },
     {
-        jpg: '../img/block-presentation/slider/slide2.jpg',
-        webp: '../img/block-presentation/slider/slide2-w.webp',
+        jpg: 'img/block-presentation/slider/slide2.jpg',
+        webp: 'img/block-presentation/slider/slide2-w.webp',
         alt: 'описание 2'
     },
     {
-        jpg: '../img/block-presentation/slider/slide3.jpg',
-        webp: '../img/block-presentation/slider/slide3-w.webp',
+        jpg: 'img/block-presentation/slider/slide3.jpg',
+        webp: 'img/block-presentation/slider/slide3-w.webp',
         alt: 'описание 3'
     },
 ];
