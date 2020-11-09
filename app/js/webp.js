b0fe9940d1da993e8c498a2722bf79820b2473fc
Aleks2904 <Garmatin171@yandex.ru>
2020-11-09 22:46:17 +0700

    v1.1

--- app/js/webp.js
+++ app/js/webp.js
@@ -10,9 +10,32 @@ document.addEventListener('DOMContentLoaded', function(){
     //block about-us
     const aboutUs = document.querySelector('.about-us__info-baner');
 
-    if(result == 'true'){
-        aboutUs.classList.add('about-us__info-baner-img-webp')
+    if(result == true){
+        aboutUs.classList.add('about-us__info-baner-img-webp');
     }else{
-        aboutUs.classList.add('about-us__info-baner-img')
+        aboutUs.classList.add('about-us__info-baner-img');
+    };
+
+    //bloack working
+
+    const slideList = document.querySelector('#js-working-slide-list').children;
+
+    for(i = 0; i < slideList.length; i++){
+        const id = slideList[i].id;
+
+        const item = document.querySelector(`#${id}`),
+              imgContainer = item.querySelector('.working__info-img');
+        let img;
+
+        if(result == true){
+            img = `../img/block-working/${id}.webp`
+        }else{
+            img = `../img/block-working/${id}.jpeg`
+        }
+
+        imgContainer.style.backgroundImage = `url(${img})`;
+
+
+        
     }
 });
